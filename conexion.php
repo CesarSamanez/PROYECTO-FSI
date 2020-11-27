9f4939c519e77dde5b0e69077d56bf8412511b96
Cesar Samanez Jimenez <csamanezj@unsa.edu.pe>
2020-11-27 07:23:34 -0500

    Conexion BD

--- conexion.php
+++ conexion.php
@@ -2,10 +2,10 @@
 
 function conectar(){
 
-    $user = "root";
-    $pass = "";
+    $user = "test";
+    $pass = "EkMGdb5c";
     $server = "localhost";
-    $db = "fsi";
+    $db = "proyecto_fsi";
     $con = mysqli_connect($server, $user, $pass) or die ("Error en conectar".mysqli_error);
 
     mysqli_select_db($con, $db);
