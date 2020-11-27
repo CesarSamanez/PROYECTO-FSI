b9dbe6c6601a42daf2223668f6da371ea29abab7
Cesar Samanez Jimenez <csamanezj@unsa.edu.pe>
2020-11-27 09:21:08 -0500

    Administracion

--- administration.php
+++ administration.php
@@ -1,12 +1,12 @@
 <!DOCTYPE html>
 <html lang="es">
 <?php
-    include("conexion.php");
+include("conexion.php");
 ?>
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <title>Mensajes</title>
     <link href='https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700' rel='stylesheet' type='text/css'>
     <link rel="stylesheet" href="css/main.css">
@@ -87,116 +87,115 @@
                 <input type="text" class="form-control input-lg" placeholder="Estoy buscado..." required="">
             </div>
             <div class="form-group">
-                <input type="text" class="form-control input-lg" placeholder="Provincia, ciudad, distrito..."
-                    required="">
+                <input type="text" class="form-control input-lg" placeholder="Provincia, ciudad, distrito..." required="">
             </div>
             <button class="btn btn-danger btn-lg" type="submit">BUSCAR</button>
         </form>
     </section>
     <!-- ====== Contenido de pagina ======-->
-    
-    <section class="full-width section" style="padding-left: 40px; padding-right: 40px;">
-            <table class="table table-hover">
-                <thead>
-                    <tr>
-                        <th scope="col ">Código</th>
-                        <th scope="col">Nombres</th>
-                        <th scope="col">Apellidos</th>
-                        <th scope="col">Género</th>
-                        <th scope="col">Dni</th>
-                        <th scope="col">Celular</th>
-                        <th scope="col">Correo</th>
-                        <th scope="col">Clave</th>
-                        <th scope="col">Función</th>
-                        <th scope="col">Estado</th>
-                        <th scope="col">Creación</th>
-                        <th scope="col">Acciones</th>
-                    </tr>
-                </thead>
-                <tbody>
-                <?php
-                    $sql = "SELECT *FROM usuario";
-                    $result = $conexion->query($sql);
 
-                    if($result->num_rows > 0){
-                        while($row = $result->fetch_assoc()){
-                            /*
+    <section class="full-width section" style="padding-left: 40px; padding-right: 40px;">
+        <table class="table table-hover">
+            <thead>
+                <tr>
+                    <th scope="col ">Código</th>
+                    <th scope="col">Nombres</th>
+                    <th scope="col">Apellidos</th>
+                    <th scope="col">Género</th>
+                    <th scope="col">Dni</th>
+                    <th scope="col">Celular</th>
+                    <th scope="col">Correo</th>
+                    <th scope="col">Clave</th>
+                    <th scope="col">Función</th>
+                    <th scope="col">Estado</th>
+                    <th scope="col">Creación</th>
+                    <th scope="col">Acciones</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $sql = "SELECT *FROM usuario";
+                $result = $conexion->query($sql);
+
+                if ($result->num_rows > 0) {
+                    while ($row = $result->fetch_assoc()) {
+                        /*
                         }
                     }
                     */
                 ?>
-                    <tr>
-                        <td><?php echo $row['codigo']; ?></td>
-                        <td><?php echo $row['nombres']; ?></td>
-                        <td><?php echo $row['apellidos']; ?></td>
-                        <td><?php echo $row['genero']; ?></td>
-                        <td><?php echo $row['dni']; ?></td>
-                        <td><?php echo $row['celular']; ?></td>
-                        <td><?php echo $row['correo']; ?></td>
-                        <td><?php echo $row['clave']; ?></td>
-                        <td><?php   
-                                    if($row['rol'] == 0){
-                                        echo "Usuario";
-                                    }else{
-                                        echo "Administrador";    
-                                    } 
-                            ?>
-                        </td>
-                        <td><?php   
-                                    if($row['estado'] == 1){
-                                        echo "Activo";
-                                    }else{
-                                        echo "Inactivo";
-                                    } 
-                            ?>
-                        </td>
-                        <td><?php echo $row['fecha_creacion']; ?></td>
-                        <td>
- 
-                            <button type="button" name="<?php echo $row['codigo']; ?>"  class="btn btn-warning">Editar</button>
-                            <button type="button" name="<?php echo $row['codigo']; ?>"class="btn btn-danger">Eliminar</button>
-                     
-                        </td>
-                    </tr>
-                    <?php
-                        }
+                        <tr>
+                            <td><?php echo $row['codigo']; ?></td>
+                            <td><?php echo $row['nombres']; ?></td>
+                            <td><?php echo $row['apellidos']; ?></td>
+                            <td><?php echo $row['genero']; ?></td>
+                            <td><?php echo $row['dni']; ?></td>
+                            <td><?php echo $row['celular']; ?></td>
+                            <td><?php echo $row['correo']; ?></td>
+                            <td><?php echo $row['clave']; ?></td>
+                            <td><?php
+                                if ($row['rol'] == 0) {
+                                    echo "Usuario";
+                                } else {
+                                    echo "Administrador";
+                                }
+                                ?>
+                            </td>
+                            <td><?php
+                                if ($row['estado'] == 1) {
+                                    echo "Activo";
+                                } else {
+                                    echo "Inactivo";
+                                }
+                                ?>
+                            </td>
+                            <td><?php echo $row['fecha_creacion']; ?></td>
+                            <td>
+                                <button type="button" name="<?php echo $row['codigo']; ?>" href="editUser.php?codigo=<?php echo $row['codigo']; ?>" class="btn btn-warning">Editar</button>
+                                <button type="button" name="<?php echo $row['codigo']; ?>" href="google.com" class="btn btn-danger">Eliminar</button>
+                            </td>
+                        </tr>
+                <?php
                     }
-                    ?>
-                </tbody>
-            </table>
-      
+                }
+                ?>
+            </tbody>
+        </table>
+
     </section>
     <!-- ====== Pie de pagina ======-->
-	<footer class="full-width footer">
-		<h4 class="text-light text-center"><b>Proyecto Final Fundamentos de Sistemas de Información</b></h4>
-		<ul class="list-unstyled fullwidth text-center footer-app-store">
-			<li>
-				<a href="#!">
-					<i class="fa fa-university" aria-hidden="true"></i>Universidad Nacional de San
-					Agustín
-				</a>
-			</li>
-			<li>
-				<a href="#!">
-					<i class="fa fa-book" aria-hidden="true"></i> Escuela Profesional de Ingeniería de
-					Sistemas
-				</a>
-			</li>
-		</ul>
+    <footer class="full-width footer">
+        <h4 class="text-light text-center"><b>Proyecto Final Fundamentos de Sistemas de Información</b></h4>
+        <ul class="list-unstyled fullwidth text-center footer-app-store">
+            <li>
+                <a href="#!">
+                    <i class="fa fa-university" aria-hidden="true"></i>Universidad Nacional de San
+                    Agustín
+                </a>
+            </li>
+            <li>
+                <a href="#!">
+                    <i class="fa fa-book" aria-hidden="true"></i> Escuela Profesional de Ingeniería de
+                    Sistemas
+                </a>
+            </li>
+        </ul>
 
-		<br>
-		<div class="container">
-			<div class="col-xs-12">
-				<ul class="list-unstyled text-center full-width footer-copyright">
-					<li>&copy; 2020 Copyright</li>
-					<li><a href="#!">Equipo de Desarrollo</a></li>
-					<li><a href="#!">Repositorio</a></li>
-				</ul>
-			</div>
-		</div>
-	</footer>
+        <br>
+        <div class="container">
+            <div class="col-xs-12">
+                <ul class="list-unstyled text-center full-width footer-copyright">
+                    <li>&copy; 2020 Copyright</li>
+                    <li><a href="#!">Equipo de Desarrollo</a></li>
+                    <li><a href="#!">Repositorio</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-    <script>window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')</script>
+    <script>
+        window.jQuery || document.write('<script src="js/jquery-1.11.2.min.js"><\/script>')
+    </script>
     <script src="js/bootstrap.min.js"></script>
     <script src="js/jquery.mCustomScrollbar.concat.min.js"></script>
     <script src="js/main.js"></script>
