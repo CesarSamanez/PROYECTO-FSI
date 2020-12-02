b1afe45c1a35e9353086536877fd033105998981
Cesar Samanez Jimenez <csamanezj@unsa.edu.pe>
2020-12-01 21:02:24 -0500

    Error sesion

--- validar.php
+++ validar.php
@@ -1,20 +1,24 @@
 <?php
-
 include("conexion.php");
-session_start();
 
-$email = $_POST['email'];
-$password = $_POST['password'];
+$correo = $_POST['email'];
+$clave = $_POST['password'];
 
-$sql = "SELECT *FROM usuario WHERE correo='$email' AND clave='$password'";
+$sql = "SELECT *FROM usuario WHERE correo='$correo' and clave='$clave'";
 $query = mysqli_query($conexion, $sql);
 
 $filas = mysqli_num_rows($query);
 
 if($filas > 0){
-    echo "window.location='index.html';";
+    echo "<script>
+        alert('Inicio de sesion exitoso');
+        </script>
+    ";
 }else{ 
-    echo "Sesion invalida";
+    echo "<script>
+        alert('Correo o clave incorrecta');
+        window.location = 'login.html';
+        </script>
+    ";
 }
-
 ?>
