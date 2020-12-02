3ec252becd1cb9599e39b7fa991a3e16f369df29
Cesar Samanez Jimenez <csamanezj@unsa.edu.pe>
2020-12-01 20:32:48 -0500

    sesion

--- validar.php
+++ validar.php
@@ -9,9 +9,10 @@ $password = $_POST['password'];
 $sql = "SELECT *FROM usuario WHERE correo='$email' AND clave='$password'";
 $query = mysqli_query($conexion, $sql);
 
-if($count = $query->num_rows > 0){
+if($query->num_rows > 0){
     header("Location: www.google.com");
 }else{
+    
     echo "Sesion invalida";
 }
 
