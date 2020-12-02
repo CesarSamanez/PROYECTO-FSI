c697b5f36a4449f9c9abf7198b0d2d95cd02ca92
Cesar Samanez Jimenez <csamanezj@unsa.edu.pe>
2020-12-01 21:37:47 -0500

    Sesion

--- validarCliente.php
+++ validarCliente.php
@@ -6,13 +6,13 @@ session_start();
 if (empty($_SESSION)) {
     echo "<script>";
     echo "alert('Debe iniciar sesión');";
-    echo "window.location = 'index.php';";
+    echo "window.location = 'index.html';";
     echo "</script>";
 } else {
     if ($_SESSION["estado"] == 0) {
         echo "<script>";
         echo "alert('Su cuenta ha sido deshabilitada, contactese con el administrador.');";
-        echo "window.location = 'admin_index.php';";
+        echo "window.location = 'index.html';";
         echo "</script>";
     }
 }
