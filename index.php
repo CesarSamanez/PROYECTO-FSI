bf46d982aab1d5639936e1e77b4a4358e3bbec1c
Cesar Samanez Jimenez <csamanezj@unsa.edu.pe>
2020-12-14 10:46:32 -0500

    Modificaciones

--- index.php
+++ index.php
@@ -172,7 +172,7 @@ include("conexion.php");
 									<?php
 									echo '<img src="data:image/png;base64,' . base64_encode($row['FOTO_INMUEBLE_DETALLES']) . '" alt=""/>';
 									?>
-									<h3><a href="post.php?codigo=<?php echo $row['CODIGO_INMUEBLE']; ?>" class="full-width post-info-title">Ver anuncio</a></h3>
+									<h3><a href="post.php?codigo=<?php echo $row['CODIGO_INMUEBLE']; ?>">Ver anuncio</a></h3>
 								</center>
 							</div>
 					<?php
